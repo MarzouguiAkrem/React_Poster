e669c51b85e4c4cd3485bf68faefa556bdf54860
Marzougui Akrem <marzougui.akrem.pro@gmail.com>
2024-11-05 14:57:56 +0100

    add form

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from './Post'
 import classes from './PostList.module.css';
-
+import NewPost from './NewPost';
 function PostList() {
     return (
+        <>
+        <NewPost/>
         <ul className={classes.posts}>
           <Post author ="Akrem" body=" Ractjs is awsome!"/>
           <Post author ="Ayhem" body=" check out the full course!!"/>
-    </ul>
+        </ul>
+        </>
+        
     )
      
 
